1033e992371e190ce3bb8599fbf8aa31db7e16ca
yandaki <boayves@gmail.com>
2023-10-04 01:10:24 +0000

    config urls

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    category = models.ForeignKey(Category , on_delete=models.PROTECT , default=1 )
+    
---
+++ blog_api/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path , include
+# from .views import PostList , PostDetail
+
+app_name = "blog_api"
+
+urlpatterns = [
+#     path('<int:pk>/' , PostDetail.as_view() , name="detailcreate"),
+#     path('' , PostList.as_view() , name="listcreate")
+]
