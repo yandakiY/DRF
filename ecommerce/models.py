93e0ee493b23a78a7ebbd31674ccf5f25634f734
yandaki <boayves@gmail.com>
2023-10-11 11:40:29 +0000

    add new model Order

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -4,6 +4,8 @@ from django_extensions.db.models import (
     TitleSlugDescriptionModel , ActivatorModel , TimeStampedModel
 )
 
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -30,3 +32,18 @@ class Product(TitleSlugDescriptionModel , ActivatorModel , TimeStampedModel):
     def __str__(self) -> str:
         return f'{self.title}'
     
+    
+    
+    
+    
+    
+
+class Order(TimeStampedModel , ActivatorModel):
+    
+    # property
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product , on_delete=models.CASCADE)
+    
+    
+    def __str__(self) -> str:
+        return f'{self.user.username} - {self.product.title}'
