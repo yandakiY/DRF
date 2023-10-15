4f562a604783b48ccf1112b7163461e08ebd2341
yandaki <boayves@gmail.com>
2023-10-15 23:11:32 +0000

    add django cors header

--- core/settings.py
+++ core/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_extensions',
     'ecommerce',
-    'ecommerce_api'
+    'ecommerce_api',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,10 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny"
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
